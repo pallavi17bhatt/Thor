a96ce91040ecc637b428a340da0f0c3ed9cccd21
pallavi bhatt <pallavibhatt@pallavis-MacBook-Pro.local>
2019-04-22 15:43:15 +0530

    used okhttp to make API call

--- app/src/main/java/com/example/thor/MainActivity.java
+++ app/src/main/java/com/example/thor/MainActivity.java
@@ -2,6 +2,18 @@ package com.example.thor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +22,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onBtnClicked(View view) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url("http://worldclockapi.com/api/json/est/now").build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.e("failure:", e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                final String data = response.body().string();
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        changeMessage(data);
+                    }
+                });
+            }
+        });
+    }
+
+    public void changeMessage (String data) {
+        TextView tv = (TextView) findViewById(R.id.textView2);
+        tv.setText(data);
+    }
+
+
+
+
 }
